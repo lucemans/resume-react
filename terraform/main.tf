3eac6b11eb62b22899b75810755aa05a43407c7e
Lucemans <luc@lucemans.nl>
2020-12-29 06:36:03 +0100

    build: :wrench: Terraform Deployment

---
+++ terraform/main.tf
@@ -0,0 +1,81 @@
+provider "kubernetes" {
+
+}
+
+resource "kubernetes_pod" "resume" {
+  metadata {
+    name      = "resume"
+    namespace = "lvksh"
+    labels = {
+      app = "resume"
+    }
+  }
+
+  spec {
+    container {
+      image = "lvk.sh/resume:latest"
+      name  = "resume"
+
+      port {
+        container_port = 80
+      }
+
+      liveness_probe {
+        http_get {
+          path = "/"
+          port = 80
+        }
+
+        initial_delay_seconds = 3
+        period_seconds        = 3
+      }
+    }
+    image_pull_secrets {
+      name = "regcred"
+    }
+  }
+}
+
+resource "kubernetes_service" "resume" {
+  metadata {
+    name      = "resume-service"
+    namespace = "lvksh"
+  }
+
+  spec {
+    selector = {
+      app = "resume"
+    }
+    type = "ClusterIP"
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
+
+resource "kubernetes_ingress" "resume" {
+  metadata {
+    name      = "resume"
+    namespace = "lvksh"
+    annotations = {
+      "traefik.ingress.kubernetes.io/router.tls" = "true"
+      "traefik.ingress.kubernetes.io/router.tls.certresolver" = "letsencrypt"
+      "traefik.ingress.kubernetes.io/priority" = "4"
+    }
+  }
+  spec {
+      rule {
+        host = "resume.lvk.sh"
+        http {
+          path {
+            path = "/"
+            backend {
+              service_name = "resume-service"
+              service_port = 80
+            }
+          }
+        }
+      }
+  }
+}
