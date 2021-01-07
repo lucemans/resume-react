7ee6f4abb55cff6bc3ffd0b9f11a959c6a168877
Lucemans <luc@lucemans.nl>
2021-01-07 07:35:12 +0100

    feat: :mute: Removed Annoying logging

--- src/util/array.horisort.ts
+++ src/util/array.horisort.ts
@@ -7,32 +7,25 @@ export function horisort<T>(list: T[], columns: number): T[] {
 }
 
 export function horisort2<T>(list: T[], columns: number): T[] {
-
     list = JSON.parse(JSON.stringify(list));
 
     const ret = [];
     const inRow = Math.floor(list.length / columns);
 
-    console.log('inRow ', inRow);
-
     let f = 0;
     while (list.filter((a) => (!!a)).length > 0) {
         for (let i = 0; i < columns; i++) {
-            if (list.length > f+i*inRow) {
-                if (list[f+i*inRow] == null) {
+            if (list.length > f + i * inRow) {
+                if (list[f + i * inRow] == null)
                     continue;
-                }
-                console.log('Adding ', list[f+i*inRow]);
-                ret.push(list[f+i*inRow]);
-                list[f + i*inRow] = null;
-                // ret.push(list[i*inRow]);
+                ret.push(list[f + i * inRow]);
+                list[f + i * inRow] = null;
             } else {
-                console.log('h',f,i);
+                console.log('h', f, i);
                 list[f] = null;
             }
         }
         f++;
     }
-
     return ret;
 }
