6502a7aaee3d7e6d371dba557db3eade7fce5441
Lucemans <luc@lucemans.nl>
2021-01-07 03:57:36 +0100

    feat: :art: Commit Shortening and Link Implementation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
+export const VERSION = process.env.PROJECT_VERSION;
+
 export default function Footer(): JSX.Element {
     
     return (
         <div className="footer">
             <div>Made by <a href="https://resume.lvk.sh">Luc van Kampen</a></div>
-            <div className="version">Version { process.env.PROJECT_VERSION || 'Unknown' }</div>
+            <a className="version" href={"https://github.com/lucemans/resume-react/commit/" + VERSION}>Version { VERSION.substr(0, 7) || 'Unknown' }</a>
         </div>
     );
 }
