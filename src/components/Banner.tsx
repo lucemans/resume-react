6f4a936feb4beb40b445a047d81cf22adc2906a9
Lucemans <luc@lucemans.nl>
2021-01-08 21:20:22 +0100

    chore: :art: Temporary Banner Start

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 
 export default function Banner(): JSX.Element {
+
+    const [clicked, setClicked] = React.useState(!!localStorage.getItem('resume_banner') || false);
     
     return (
-        <div className="">
+        <div className={clicked ? 'banner hide' : 'banner'}>
+
+            <div className="inside">
+                
+
+                <div className="button">Understood</div>
+            </div>
             {/* <div>Made by <a href="https://resume.lvk.sh">Luc van Kampen</a></div> */}
         </div>
     );
